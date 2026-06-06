49c2e106ada3cbff0afe1d8782e800af7958d8e8
hiromia006 <hiromia006@gmail.com>
2026-06-06 21:42:16 +0600

    Rename Alerts class to AlertsTest and add ExplicitWait and ImplicitlyWaitTest classes for improved Selenium alert handling

--- src/test/java/selenium/class_two/AlertsTest.java
+++ src/test/java/selenium/class_two/AlertsTest.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class Alerts {
+public class AlertsTest {
     public static void main(String[] args) throws InterruptedException {
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.tutorialspoint.com/selenium/practice/alerts.php");
