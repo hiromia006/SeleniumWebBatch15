454efa3445472738d5bb3e2dae03fcba70f94a80
hiromia006 <hiromia006@gmail.com>
2026-06-09 21:43:45 +0600

    Add BasicTest class with initial TestNG tests and configure build.gradle for TestNG support

---
+++ src/test/java/selenium/testng_one/BasicTest.java
@@ -0,0 +1,27 @@
+package selenium.testng_one;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class BasicTest {
+    @Test(description = "Test 1", priority = 1)
+    public void test1() {
+        System.out.println("Test 1");
+        Assert.assertEquals("Hello", "Hello", "sddsf1");
+        System.out.println("Test 2");
+    }
+
+    @Test(description = "Test 2", priority = 0)
+    public void test2() {
+        System.out.println("Test 21");
+        Assert.assertEquals("Hello", "Hello", "sddsf2");
+        System.out.println("Test 22");
+    }
+
+    @Test(description = "Test 2", priority = 2)
+    public void atest() {
+        System.out.println("Test 21");
+        Assert.assertEquals("Hello", "Hello", "sddsf2");
+        System.out.println("Test 22");
+    }
+}
